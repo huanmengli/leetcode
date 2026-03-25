cf5193e11b590909d5c16d2b62815c617f9f3587
Admin <18266753850@163.com>
2026-03-26 00:00:57 +0800

    ListNode测试1.0

--- src/com/test/listNode/ListNodeTest.java
+++ src/com/test/listNode/ListNodeTest.java
@@ -6,11 +6,14 @@ class ListNode{
     ListNode(int x){
         this.val = x;
     }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 
 public class ListNodeTest {
     public static void main(String[] args) {
-        ListNode node = new ListNode(1);
-        System.out.println(node.next);
+        ListNode node1 = new ListNode(1);
+        ListNode node2=new ListNode(2,node1);
+        ListNode node3=new ListNode(3,node2);
+        System.out.println(node3.val);
     }
 }
