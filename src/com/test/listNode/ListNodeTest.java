898ac5089feec374fe2500e6795e16bd575711af
Admin <18266753850@163.com>
2026-03-26 21:54:03 +0800

    ListNode测试2.0

--- src/com/test/listNode/ListNodeTest.java
+++ src/com/test/listNode/ListNodeTest.java
@@ -1,5 +1,7 @@
 package com.test.listNode;
 
+import java.util.List;
+
 class ListNode{
     int val;
     ListNode next=null;
@@ -14,9 +16,22 @@ public class ListNodeTest {
         ListNode node1 = new ListNode(1);
         ListNode node2=new ListNode(2,node1);
         ListNode node3=new ListNode(3,node2);
-        System.out.println(node3.val);
+        System.out.println(node3.next.next.val);
+//        ListNode node4 = new ListNode(1);
+//        ListNode node5=new ListNode(2,node4);
+//        ListNode node6=new ListNode(3,node5);
+//        System.out.println(addTwoNumbers(node3,node6).val);
     }
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode node = null;
+        ListNode nodeTrue=new ListNode(0);
+        while (nodeTrue!=null){
+            if (l1.next== null&& l2.next== null){
+                break;
+            }
+            ListNode nodeTian=new ListNode(l1.val+l2.val);
+
+        }
         return null;
     }
 }
